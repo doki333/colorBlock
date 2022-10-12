fb0d760db64ac5509d2d6e29c317097ea8fd3b6f
doki333 <duckky333@gmail.com>
2022-10-13 03:27:55 +0900

    Initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,29 @@
+const numArr = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
+
+const App = () => {
+  return (
+    <div>
+      <h1>TIMELINE</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>체크</th>
+            <th>시간</th>
+            <th>할일</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+          </tr>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default App;
